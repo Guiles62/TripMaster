f76da5f67a92f7d487d428624e9840ec9b9b0fbc
Guiles62 <https://github.com/Guiles62/P4.git>
2022-03-23 10:38:27 +0100

    TourGuideService changes

--- gpsUtil/src/main/java/gpsUtil/GpsUtilApplication.java
+++ gpsUtil/src/main/java/gpsUtil/GpsUtilApplication.java
@@ -2,7 +2,7 @@ package gpsUtil;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
 
 @SpringBootApplication
 public class GpsUtilApplication {
