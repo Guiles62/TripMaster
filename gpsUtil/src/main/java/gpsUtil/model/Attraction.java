39bd64fa044a6567ea4b25d31328ec072a674efd
Guiles62 <https://github.com/Guiles62/P4.git>
2022-04-11 11:24:50 +0200

    add Model in GpsUtil

---
+++ gpsUtil/src/main/java/gpsUtil/model/Attraction.java
@@ -0,0 +1,59 @@
+package gpsUtil.model;
+
+
+
+import java.util.UUID;
+
+public class Attraction extends Location {
+    public String attractionName;
+    public String city;
+    public String state;
+    public UUID attractionId;
+
+    public Attraction(String attractionName, String city, String state, UUID attractionId) {
+        this.attractionName = attractionName;
+        this.city = city;
+        this.state = state;
+        this.attractionId = attractionId;
+    }
+
+    public Attraction(double latitude, double longitude, String attractionName, String city, String state, UUID attractionId) {
+        super(latitude, longitude);
+        this.attractionName = attractionName;
+        this.city = city;
+        this.state = state;
+        this.attractionId = attractionId;
+    }
+
+    public String getAttractionName() {
+        return attractionName;
+    }
+
+    public void setAttractionName(String attractionName) {
+        this.attractionName = attractionName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public UUID getAttractionId() {
+        return attractionId;
+    }
+
+    public void setAttractionId(UUID attractionId) {
+        this.attractionId = attractionId;
+    }
+}
